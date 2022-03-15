a0f068bf6aa727547d9bb95139e4b78197813ba4
Jenna <sjieun11@gmail.com>
2022-03-15 14:12:28 -0600

    updated design feature

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import { SearchArea } from "./components/SearchArea";
 import { getWeather } from "./components/getWeather";
 import { format, addMinutes, addSeconds } from "date-fns";
 import ReactCountryFlag from "react-country-flag";
+import "./App.css";
 
 const App = () => {
   const [cityText, setCityText] = useState("");
+
+  const [weatherData, setWeatherData] = useState({
+    cityName: "",
+    countryCode: "",
+    date: "",
+    time: "",
+    temperature: "",
+    weather: "",
+    weatherMain: "",
+    icon: "",
+  });
 
   const weatherDataFnc = (res) => {
     const date = new Date();
@@ -31,45 +43,35 @@ const App = () => {
     getWeather("denver").then(weatherDataFnc);
   }, []);
 
-  const [weatherData, setWeatherData] = useState({
-    cityName: "",
-    countryCode: "",
-    date: "",
-    time: "",
-    temperature: "",
-    weather: "",
-    weatherMain: "",
-    icon: "",
-  });
-
   const iconUrl = `http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
 
   const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
 
   return (
-    <div>
-      <p>
-        {weatherData.cityName}, {regionNames.of(weatherData.countryCode)}{" "}
-        <ReactCountryFlag
-          countryCode={weatherData.countryCode}
-          svg
-          style={{
-            width: "2em",
-            height: "2em",
-          }}
-          title={weatherData.countryCode}
-        />
-      </p>
-
-      <p>{weatherData.date}</p>
-      <p>{weatherData.time}</p>
-      <p>{weatherData.temperature}</p>
-      <p>
-        {weatherData.weather}
-        <img src={iconUrl} />
-      </p>
-
+    <div className="App">
+      <div className="box">
+        <p style={{ fontSize: "30px" }}>
+          {weatherData.cityName}, {regionNames.of(weatherData.countryCode)}{" "}
+          <ReactCountryFlag
+            countryCode={weatherData.countryCode}
+            svg
+            style={{
+              width: "2em",
+              height: "2em",
+            }}
+            title={weatherData.countryCode}
+          />
+        </p>
+        <p style={{ fontSize: "25px" }}>{weatherData.date}</p>
+        <p style={{ fontSize: "25px" }}>{weatherData.time}</p>
+        <p className="tem">{weatherData.temperature}</p>
+        <p className="weatherIcon" style={{ fontSize: "25px" }}>
+          {weatherData.weather}
+          <img src={iconUrl} />
+        </p>
+      </div>
       <SearchArea
+        className="search"
         value={cityText}
         onSubmit={(text) => {
           console.log(text);
